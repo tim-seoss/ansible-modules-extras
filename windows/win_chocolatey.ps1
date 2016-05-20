4198c0a62620eec4985d274f3aaad884cf6a35e2
Tim Small <tim@seoss.co.uk>
2016-05-20 18:29:32 +0000

    Allow parameters to be passed for uninstall (required by some packages)

--- windows/win_chocolatey.ps1
+++ windows/win_chocolatey.ps1
@@ -284,6 +284,11 @@ Function Choco-Uninstall
         $cmd += " -force"
     }
 
+    if ($packageparams)
+    {
+        $cmd += " -params '$packageparams'"
+    }
+
     $results = invoke-expression $cmd
 
     if ($LastExitCode -ne 0)
